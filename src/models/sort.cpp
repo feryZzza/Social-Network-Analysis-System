44ba70cef54460d38483d05ba2982ba63fc654e9
www-laojiumen-com <996147533@qq.com>
2025-11-12 23:25:08 +0800

    Update sort.cpp

--- src/models/sort.cpp
+++ src/models/sort.cpp
@@ -1,3 +1,13 @@
 #include "models/sort.h"
 
+//  自定义工具函数 
+template <typename T>
+void mySwap(T& a, T& b) {
+    T temp = std::move(a);
+    a = std::move(b);
+    b = std::move(temp);
+}
 
+int myMin(int a, int b) {
+    return a < b ? a : b;
+}
