4916b5f06e6229db60786578bae6fd3a46bdaa4b
www-laojiumen-com <996147533@qq.com>
2025-11-12 23:17:41 +0800

    Update sort.h
    添加排序算法的说明

--- include/models/sort.h
+++ include/models/sort.h
@@ -8,3 +8,31 @@
 #include "models/clients.h"
 #include "models/Post.h"
 #include "models/comment.h"
+
+
+// 比较函数前置声明
+bool compareHaoyou(const client& a, const client& b);
+bool comparePost(const Post& a, const Post& b);
+
+// 排序算法前置声明
+template <typename T>
+void insertionSort(SeqList<T>& arr, bool (*count)(const T& a, const T& b));
+
+template <typename T>
+int partition(SeqList<T>& arr, int left, int right, bool (*count)(const T& a, const T& b));
+
+template <typename T>
+void quickSort(SeqList<T>& arr, int left, int right, bool (*count)(const T& a, const T& b));
+
+template <typename T>
+void heapify(SeqList<T>& arr, int n, int i, bool (*count)(const T& a, const T& b));
+
+template <typename T>
+void heapSort(SeqList<T>& arr, bool (*count)(const T& a, const T& b));
+
+template <typename T>
+void ShiyingSort(SeqList<T>& arr, bool (*count)(const T& a, const T& b));
+
+// 排行榜核心函数前置声明
+void haoyoushu(SeqList<client>& clients);
+void dianzanshu(SeqList<client>& users);
